05bbf5f17f9c0435f2c892f66a4cf3f79c652a30
Tunahan Özcan <tunahan.ozcan@tesan.com.tr>
2021-10-07 19:41:26 +0300

    Error, About sayfaları eklendi.

--- BussinessLayer/Concrete/CommentManager.cs
+++ BussinessLayer/Concrete/CommentManager.cs
@@ -20,7 +20,7 @@ namespace BussinessLayer.Concrete
 
         public void CommentAdd(Comment comment)
         {
-            throw new NotImplementedException();
+            _commentDal.Insert(comment);
         }
 
         public List<Comment> GetList(int id)
---
+++ BussinessLayer/Concrete/NewsLetterManager.cs
@@ -0,0 +1,26 @@
+﻿using BussinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+   public class NewsLetterManager:INewsLatterService
+    {
+        INewsLetterDal _newsLetterDal;
+
+        public NewsLetterManager(INewsLetterDal newsLetterDal)
+        {
+            _newsLetterDal = newsLetterDal;
+        }
+
+        public void AddNewsLetter(NewsLetter newsLetter)
+        {
+            _newsLetterDal.Insert(newsLetter);
+        }
+    }
+}
