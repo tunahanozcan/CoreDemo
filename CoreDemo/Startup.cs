17bef8ba4e16b62cce3e9077c65601a64d6b11c7
Tunahan Özcan <tunahan.ozcan@tesan.com.tr>
2021-11-04 20:42:30 +0300

    Yazar dashboard güncellemeleri yapıldı.

--- CoreDemo/Startup.cs
+++ CoreDemo/Startup.cs
@@ -27,7 +27,7 @@ namespace CoreDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            //services.AddSession();
+           // services.AddSession();
             services.AddMvc(config =>
             {
                 var policy = new AuthorizationPolicyBuilder()
