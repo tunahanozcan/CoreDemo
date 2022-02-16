f98e43d027a5511b86b0d02f80d029ffa4c0cbd0
Tunahan Özcan <tunahan.ozcan@tesan.com.tr>
2022-02-16 18:17:06 +0300

    Farklı db de yer alan api katmanına bağlantı sağlandı.

---
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,78 @@
+﻿using BlogApiDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+            using var c = new Context();
+            var values = c.Employees.ToList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult EmployeeAdd(Employee employee)
+        {
+            using var c = new Context();
+            c.Add(employee);
+            c.SaveChanges();
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetEmployeeById(int id)
+        {
+            using var c = new Context();
+            var employee = c.Employees.Find(id);
+            if (employee==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(employee);
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            using var c = new Context();
+            var employee = c.Employees.Find(id);
+            if (employee==null)
+            {
+                return NotFound($"{id}'li çalışan bulunamadı!");
+            }
+            else
+            {
+                c.Remove(employee);
+                c.SaveChanges();
+                return Ok($"{employee.Name} başarıyla silindi.");
+            }
+        }
+        [HttpPut]
+        public IActionResult UpdateEmployee(Employee employee)
+        {
+            using var c = new Context();
+            var emp = c.Find<Employee>(employee.Id);
+            if (emp is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                emp.Name = employee.Name;
+                c.Update(emp);
+                c.SaveChanges();
+                return Ok();
+            }
+        }
+    }
+}
---
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,80 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+        public IActionResult GerWriterById(int writerid)
+        {
+            var findWriter = writers.FirstOrDefault(x => x.Id == writerid);
+            var jsonWriters = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriters);
+        }
+
+        [HttpPost]
+        public IActionResult AddWriter(WriterClass w)
+        {
+            if (w.Id!=0 &&w.Name!=null)
+            {
+                writers.Add(w);
+                var jsonWriters = JsonConvert.SerializeObject(w);
+                return Json(jsonWriters);
+            }
+            return BadRequest("Yazar bilgisi boş olamaz!");
+        }
+
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == id);
+            if (writer!=null)
+            {
+                writers.Remove(writer);
+                return Ok(writer);
+            }
+            return BadRequest("belirtilen id li yazar bulunamadı!");
+        }
+
+        public IActionResult UpdateWriter(WriterClass w)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == w.Id);
+            writer.Name = w.Name;
+            var jsonWriter = JsonConvert.SerializeObject(writer);
+            return Json(jsonWriter);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+        new WriterClass
+        {
+            Id=1,
+            Name="Ayşe"
+        },
+        new WriterClass
+        {
+            Id=2,
+            Name="Ahmet"
+        },
+        new WriterClass
+        {
+            Id=3,
+            Name="Buse"
+        }
+        };
+    }
+}
